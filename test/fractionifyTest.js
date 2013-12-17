6cbd15b320fee62b824dee41aeef8cc0015a1cc5
JeremyCarlsten <jeremy@jeremycarlsten.com>
2013-12-17 07:23:02 -0600

    Wrapping up test coverage

--- test/fractionifyTest.js
+++ test/fractionifyTest.js
@@ -9,19 +9,15 @@ test("Test set value on object", function () {
 
     $fractionify.fractionify();
 
-    deepEqual($fractionify.val(), "1/4");
+    deepEqual($fractionify.val(), "1/4", "Fraction set on object's value");
 
 });
 test("Test HappyPath calculations", function () {
-    deepEqual(getFraction(.25), "1/4");
-    deepEqual(getFraction(.5), "1/2");
-    deepEqual(getFraction(.0625), "1/16");
-
-});
-
-
-test("Happy Path tiny numerator To Fraction", function () {
-    deepEqual(getFraction(.123456789), 1);
-    deepEqual(getFraction(.000000009), 1);
-    deepEqual(getFraction(.9999999999), 1);
+    deepEqual(getFraction(.25), "1/4", "Calculate 1/4th");
+    deepEqual(getFraction(.5), "1/2", "Calculate 1/2");
+    deepEqual(getFraction(.0625), "1/16", "Calculate 1/16th");
+    deepEqual(getFraction(.123456789), "12.3456789/100", "Calculate long decimal '.123456789'");
+    deepEqual(getFraction(.9999999999), "99.99999998999999/100", "Calculate almost 1");
+    deepEqual(getFraction(.000000009), "9e-7/100", "Calculate very small decimal '.000000009'");
+    deepEqual(getFraction(1.5), "3/2", "Calculate greater than one decimal '1.5'")
 });
--- test/greatestCommonFactorTest.js
+++ test/greatestCommonFactorTest.js
@@ -1,17 +1,17 @@
 
 test("Test greatest common denominator happyPath", function () {
-    deepEqual(calculateGreatestCommonFactor(25,100), 25);
-    deepEqual(calculateGreatestCommonFactor(6.25,100), 6.25);
-    deepEqual(calculateGreatestCommonFactor(123456,100), 4);
-    deepEqual(calculateGreatestCommonFactor(95,100), 5);
+    deepEqual(calculateGreatestCommonFactor(25,100), 25, '25/100');
+    deepEqual(calculateGreatestCommonFactor(6.25,100), 6.25, "6.25/100");
+    deepEqual(calculateGreatestCommonFactor(123456,100), 4, "123456/100");
+    deepEqual(calculateGreatestCommonFactor(95,100), 5, "95/100");
 });
 
 test("Test no greatestCommon Factor", function () {
-    deepEqual(calculateGreatestCommonFactor(1.23456,100), 1);
-    deepEqual(calculateGreatestCommonFactor(1.789456,100), 1);
-    deepEqual(calculateGreatestCommonFactor(0.23456,100), 1);
-    deepEqual(calculateGreatestCommonFactor(0.123456,100), 1);
-    deepEqual(calculateGreatestCommonFactor(0.123456789,100), 1);
+    deepEqual(calculateGreatestCommonFactor(1.23456,100), 1, "large number without a GCF");
+    deepEqual(calculateGreatestCommonFactor(1.789456,100), 1,"large number without a GCF");
+    deepEqual(calculateGreatestCommonFactor(0.23456,100), 1,"small number without a GCF");
+    deepEqual(calculateGreatestCommonFactor(0.123456,100), 1,"small number without a GCF");
+    deepEqual(calculateGreatestCommonFactor(0.123456789,100), 1, "small number without a GCF");
 });
 
 
